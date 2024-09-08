44cfbc8effd20edecea2f8894911ac8c698024a2
lamdv <lam.dv@live.com>
2024-09-08 21:51:52 +0700

    update form placement

--- scripts/main.js
+++ scripts/main.js
@@ -111,3 +111,21 @@ $(function(){
 $(function(){
   $("#rsvp").load("form.html"); 
 });
+
+$("form").submit(function () {
+
+  var this_master = $(this);
+
+  this_master.find('input[type="checkbox"]').each( function () {
+      var checkbox_this = $(this);
+
+
+      if( checkbox_this.is(":checked") == true ) {
+          checkbox_this.attr('value','1');
+      } else {
+          checkbox_this.prop('checked',true);
+          //DONT' ITS JUST CHECK THE CHECKBOX TO SUBMIT FORM DATA    
+          checkbox_this.attr('value','0');
+      }
+  })
+})
