8fec976511085da36f8626b40657576af1710ecf
lamdv <lam.dv@live.com>
2024-09-13 14:38:11 +0700

    update backend

--- scripts/form.js
+++ scripts/form.js
@@ -1,12 +1,20 @@
 form = document.querySelector("#form")
 submitButton = document.querySelector("#submit")
-// scriptURL = 'https://script.google.com/macros/s/AKfycbwG9vCMBREFM4suhSiTdVPFu7-F-6JclKyZGGuKjFS-dqaZT6kKXS6r_15kub3YH2R5yw/exec'
-scriptURL = 'https://api.web3forms.com/submit'
+scriptURL = 'https://script.google.com/macros/s/AKfycbwPYhT5hqT498XaTPNB6zlPvOA5FUfj0t7LFKc7tB_QyKSMPhV7YWBwHFhVk_MDmFF_dA/exec'
+// https://script.google.com/macros/s/AKfycbx5ELgHlKRdsv2CO9CqQceYs16Q9vB0C7yni1g6FRnUaN7wg7Ll4e14o3slu9Zp-3xtJA/exec
+// scriptURL = 'https://api.web3forms.com/submit'
 
 form.addEventListener('submit', e => {
     submitButton.disabled = true
     e.preventDefault()
     let requestBody = new FormData(form)
+    if (requestBody.get('rsvp')===null){
+        requestBody.set('rsvp', '0')
+        requestBody.set('guest', '0')
+    }
+    else if (requestBody.get('guest') === null) {
+        requestBody.set('guest', '0')
+    }
     fetch(scriptURL, { method: 'POST', body: requestBody})
     .then(response => {
         alert('Cảm ơn bạn đã đến dự đám cưới của chúng mình! Hi vọng được gặp mọi người!', response)
