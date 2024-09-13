55da48415186734cbac042ecfbb9a34ec4423214
lamdv <lam.dv@live.com>
2024-09-13 16:13:41 +0700

    update style

--- scripts/form.js
+++ scripts/form.js
@@ -4,6 +4,10 @@ scriptURL = 'https://script.google.com/macros/s/AKfycbwPYhT5hqT498XaTPNB6zlPvOA5
 // https://script.google.com/macros/s/AKfycbx5ELgHlKRdsv2CO9CqQceYs16Q9vB0C7yni1g6FRnUaN7wg7Ll4e14o3slu9Zp-3xtJA/exec
 // scriptURL = 'https://api.web3forms.com/submit'
 
+$(document).ready(function(event){
+    form.reset()
+})
+
 form.addEventListener('submit', e => {
     submitButton.disabled = true
     e.preventDefault()
@@ -22,7 +26,15 @@ form.addEventListener('submit', e => {
     }
     fetch(scriptURL, { method: 'POST', body: requestBody})
     .then(response => {
-        alert('Cảm ơn bạn đã đến dự đám cưới của chúng mình! Hi vọng được gặp mọi người!', response)
+        if (response.status === 200) {
+            form.reset()
+        }
+        if (requestBody.get('rsvp') === '1') {
+            alert('Cảm ơn bạn dành thời gian đến dự đám cưới của chúng mình! Hi vọng được gặp mọi người!', response)
+        }
+        else {
+            alert('Cảm ơn bạn đã thông báo không tham dự! Hẹn gặp vào một dịp khác nhé!', response)
+        }
         submitButton.disabled = false
         })
     .catch(error => {
